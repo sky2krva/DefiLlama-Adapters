24f1a1ef344b2bb6332833bf6d2cd79e82404825
yuhuakukude <yuhuakukude@gmail.com>
2023-09-04 21:44:43 +0800

    add krav ethereum tvl

--- projects/krav/index.js
+++ projects/krav/index.js
@@ -22,7 +22,6 @@ Object.keys(config).forEach(chain => {
       return api.sumTokens({ tokensAndOwners: logs.map(i => [i.token, i.pool]), blacklistedTokens: KRAV?[KRAV]:[] })
     }
   }
-
   if (kravPool)
     module.exports[chain].staking = sumTokensExport({ owner: kravPool, tokens: [KRAV]})
 })
